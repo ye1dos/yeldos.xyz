4287000e01df7ecd10090e3347af301d6664d44d
ye1dos <yeldos.nurymov@nu.edu.kz>
2020-07-05 16:29:33 +0600

    first commit

---
+++ src/Components/About.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+
+    if(this.props.data){
+      var name = this.props.data.name;
+      var profilepic= "images/"+this.props.data.image;
+      var bio = this.props.data.bio;
+      var city = this.props.data.address.city;
+      var phone= this.props.data.phone;
+      var email = this.props.data.email;
+    }
+
+    return (
+      <section id="about">
+      <div className="row">
+         <div className="three columns">
+            <img className="profile-pic"  src={profilepic} alt="" />
+         </div>
+         <div className="nine columns main-col">
+            <h2>About Me</h2>
+
+            <p>{bio}</p>
+            <div className="row">
+               <div className="columns contact-details">
+                  <h2>Contact Details</h2>
+                  <p className="address">
+						   <span>{name}</span><br />
+						   <span>
+						         {city}
+                   </span><br />
+						   <span>{phone}</span><br />
+                     <span>{email}</span>
+					   </p>
+               </div>
+            </div>
+         </div>
+      </div>
+
+   </section>
+    );
+  }
+}
+
+export default About;
---
+++ src/Components/Portfolio.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Portfolio extends Component {
+  render() {
+
+    return (
+      <section id="portfolio">
+
+      <div className="row">
+
+         <div className="twelve columns collapsed">
+
+            <h1>To Be Added Soon</h1>
+
+            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+                {/* {projects} */}
+            </div>
+          </div>
+      </div>
+   </section>
+    );
+  }
+}
+
+export default Portfolio;
